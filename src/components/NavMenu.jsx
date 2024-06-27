a561c7c3ffeaa078163c27a7ddc640438c939c1d
paulshess <paulshess@icloud.com>
2024-06-27 09:50:46 +0100

    adding styling for menu tabs

---
+++ src/components/NavMenu.jsx
@@ -0,0 +1,29 @@
+import { NavLink } from "react-router-dom";
+
+export default function NavMenu() {
+	return (
+		<nav>
+			<NavLink to="/" className={({ isActive }) => (isActive ? "active" : " ")}>
+				Home
+			</NavLink>
+			<NavLink
+				to="/nasa"
+				className={({ isActive }) => (isActive ? "active" : " ")}
+			>
+				NASA
+			</NavLink>
+			<NavLink
+				to="/weather"
+				className={({ isActive }) => (isActive ? "active" : " ")}
+			>
+				Weather
+			</NavLink>
+			<NavLink
+				to="/tictactoe"
+				className={({ isActive }) => (isActive ? "active" : " ")}
+			>
+				Tic Tac Toe
+			</NavLink>
+		</nav>
+	);
+}
